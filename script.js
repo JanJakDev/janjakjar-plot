422a2818dae8b03a29f8ed64bb3e929e90ceed91
Jan Schymczyk <janjak.developer@gmail.com>
2019-10-26 14:40:11 +0200

    Itemfunktion vorbereitet!

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function kontakt(){
 	document.getElementById("kontaktB").innerHTML = "Discord: JanJakJar#3161";
 }
+
+function item(x){
+	switch(x){
+		case 1:
+			alert("Item: name\nMenge: var St.\nPreis: ?$");
+		break;
+	}
+}
