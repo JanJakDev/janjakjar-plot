504f526b95e34ad1c8b13f719ff13b4f52f323d3
Jan Schymczyk <janjak.developer@gmail.com>
2019-10-29 18:25:24 +0100

    Item updated!

--- script.js
+++ script.js
@@ -5,10 +5,25 @@ function kontakt(){
 function item(x){
 	switch(x){
 		case 1:
-			alert("Item: Ocelot Spawn Egg\nMenge: 1 St.\nPreis: 90k$");
+			alert("Item: Ozelot Spawn-Ei\nMenge: 1 St.\nPreis: 80k$");
 		break;
 		case 2:
-			alert("Item: Phorx's Kopf\nMenge: 1 St.\nPreis: 23k$");
+			alert("Item: Schildkrötenei\nMenge: 1 St.\nPreis: 350$");
+		break;
+		case 3:
+			alert("Item: Räucherofen\nMenge: 1 St.\nPreis: 100$");
+		break;
+		case 4:
+			alert("Item: Schmelzofen\nMenge: 1 St.\nPreis: 170$");
+		break;
+		case 5:
+			alert("Item: Steinsäge\nMenge: 1 St.\nPreis: 50$");
+		break;
+		case 6:
+			alert("Item: Schleifstein\nMenge: 1 St.\nPreis: 50$");
+		break;
+		case 7:
+			alert("Item: Lagerfeuer\nMenge: 1 St.\nPreis: 40$");
 		break;
 	}
 }
